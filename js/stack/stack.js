2c1162350798c89f4c997cafe9d2bc89d3333f0e
Arnaud C <djcase001@gmail.com>
2019-10-30 03:33:13 -0400

    Implements the Stack data structure

---
+++ js/stack/stack.js
@@ -0,0 +1,57 @@
+function Stack(){
+    this.top = -1;
+    this.length = 0;
+    this.empty = true;
+    this.push = push;
+    this.pop = pop;
+    this.peek = peek;
+    this.clear = clear;
+    this.dataStore = [];
+    this.print = print;
+}
+
+function clear(){
+    delete this.dataStore;
+    this.dataStore = [];
+    this.top = this.length = 0;
+}
+
+function push(element) {
+    this.dataStore[++this.top] = element;
+    this.length++;
+    this.empty = false;
+}
+
+function pop() {
+    var data = [];
+    for(i=0; i<this.length - 1; i++){
+        data[i] = this.dataStore[i];
+    }
+    this.dataStore = data;
+    this.top--;
+    this.length--;
+    this.empty = this.length ? true : false;
+}
+
+function peek() {
+   return this.dataStore[this.top];
+}
+
+function print(){
+    console.log(this.dataStore);
+}
+
+var stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+stack.print();
+console.log(stack.peek());
+stack.pop();
+stack.pop();
+stack.print();
+stack.pop();
+console.log(stack.peek());
