5c7202e1b6aa7747a3260422886527cd3e73d00f
nonotizekod <djcase001@gmail.com>
2019-11-10 19:42:32 -0500

    Updates the doubly linked list functions

--- d-linked-list/dlinkedlist.js
+++ d-linked-list/dlinkedlist.js
@@ -6,11 +6,13 @@ function Node(element) {
 
 function LList() {
     this.head = new Node("head");
+    this.tail = this.head; // I feel that the tail is useful when we have traverse the Linked list backward
     this.find = find;
     this.insert = insert;
     this.remove = remove;
     this.display = display;
     this.findPreviousNode = findPreviousNode;
+    this.displayBkWrd = displayBkWrd;
 }
 
 function find(item){
@@ -25,7 +27,9 @@ function insert(newElement, item) {
     var newNode = new Node(newElement);
     var current = this.find(item);
     newNode.next = current.next;
+    newNode.previous = current;
     current.next = newNode;
+    this.tail = newNode;
 }
 
 function display(){
@@ -45,18 +49,40 @@ function findPreviousNode(item) {
 }
 
 function remove(item){
-    var currNode = this.findPreviousNode(item);
-    currNode.next = currNode.next.next;
+    var currNode = this.find(item);
+    if (currNode.next) {
+        var prevNode = currNode.previous;
+        var nextNode = currNode.next;
+        prevNode.next = nextNode;
+        nextNode.previous = prevNode;
+    }
+    if (this.tail.element == currNode.element){
+        this.tail = currNode.previous;
+        currNode.next = null;
+        currNode.previous = null;
+    }
+}
+
+function displayBkWrd(){
+    var current = this.tail;
+    while(current.previous){
+        console.log(current.element);
+        current = current.previous;
+    }
 }
 
 var link = new LList();
-link.insert('Linda', 'head');
-link.insert('Katiana', 'Linda');
-link.insert('Sheryll', 'Katiana');
-link.insert('Sravani', 'Sheryll');
-link.insert('Danica', 'Sravani');
+link.insert('1- Linda', 'head');
+link.insert('2- Katiana', '1- Linda');
+link.insert('3- Sheryll', '2- Katiana');
+link.insert('4- Sravani', '3- Sheryll');
+link.insert('5- Danica', '4- Sravani');
 link.display();
+console.log("Display Backward...");
+link.displayBkWrd();
 console.log("Removing Sheryll...");
-link.remove('Sheryll');
+link.remove('3- Sheryll');
 link.display();
+link.remove('5- Danica');
+console.log(link.tail);
 
