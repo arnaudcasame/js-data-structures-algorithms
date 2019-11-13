1dc7158d187a8db14fb919288e348a68ff76670f
Arnaud Casamé <noon.tizekod@gmail.com>
2019-11-12 23:28:04 -0500

    Adds the functions blueprint

---
+++ hashing/hashing.js
@@ -0,0 +1,23 @@
+function HashTable(){
+    this.table = new Array(137);
+    this.put = put;
+    this.get = get;
+    this.simpleHash = simpleHash;
+    this.showDistro = showDistro;
+}
+
+function put(element) {
+    
+}
+
+function get(element) {
+    
+}
+
+function simpleHash(params) {
+    
+}
+
+function showDistro(params) {
+    
+}
